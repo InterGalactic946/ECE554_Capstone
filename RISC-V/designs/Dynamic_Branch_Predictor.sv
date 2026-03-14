ec30a9208ae21cdded735a8dca1ea0b8326a0eb2
Srivibhav-J <sjonnalagad2@wisc.edu>
2026-03-13 19:02:20 -0500

    Modified Scripts.

--- RISC-V/designs/Dynamic_Branch_Predictor.sv
+++ RISC-V/designs/Dynamic_Branch_Predictor.sv
@@ -16,7 +16,7 @@ module Dynamic_Branch_Predictor (
     input logic rst_i,  // Active high reset signal
     input xlen_t PC_curr_i,  // Current PC address
     input xlen_t IF_ID_PC_curr_i,  // Pipelined previous PC address
-    input logic [1:0] IF_ID_prediction_i,   // The predicted value of the previous branch instruction.
+    input logic [1:0] IF_ID_prediction_i,  // The predicted value of the previous branch inst.
     input logic enable_i,  // Enable signal for the DynamicBranchPredictor
     input logic wen_BTB_i,  // Write enable for BTB (Branch Target Buffer) (from the decode stage)
     input logic wen_BHT_i,  // Write enable for BHT (Branch History Table) (from the decode stage)
@@ -43,7 +43,7 @@ module Dynamic_Branch_Predictor (
   ///////////////////////////////////////////////
   logic [1:0] prediction_rd;  // The prediction read out as it is from the memory.
   state_t prev_prediction;  // Holds the previous prediction.
-  state_t updated_prediction;                        // The new prediction to be stored in the BHT on an incorrect prediction.
+  state_t updated_prediction;  // The new prediction to be stored in the BHT on an incorrect pred.
   logic updated_valid;  // The updated valid bit for the branch.
   logic valid;  // Indicates that the valid bit is set.
   logic [DBP_TAG_W-1:0] read_tag;  // The tag used for the current instruction as a read.
@@ -51,8 +51,8 @@ module Dynamic_Branch_Predictor (
   logic read_tags_match;  // Used to determine if current PC tag matches cached tag in BHT.
   logic write_tags_match;  // Used to determine if IF/ID PC tag matches cached tag in BHT.
   logic error;  // Error flag raised when prediction state is invalid.
-  logic [DBP_BHT_ENTRY_W-1:0] BHT[0:BHT_ENTRIES-1];  // Declare BHT: {tag, prediction[1:0], valid}
-  xlen_t BTB[0:BTB_ENTRIES-1];  // Declare BTB
+  logic [DBP_BHT_ENTRY_W-1:0] BHT[BHT_ENTRIES];  // Declare BHT: {tag, prediction[1:0], valid}
+  xlen_t BTB[BTB_ENTRIES];  // Declare BTB
   ////////////////////////////////////////////////
 
   ////////////////////////////////////////
@@ -68,14 +68,14 @@ module Dynamic_Branch_Predictor (
     end else begin
       // Update BHT entry if needed (for example, on a misprediction)
       if (enable_i && wen_BHT_i) begin
-        BHT[IF_ID_PC_curr_i[BHT_IDX_MSB:BHT_IDX_LSB]][DBP_BHT_ENTRY_W-1:3] <= IF_ID_PC_curr_i[XLEN-1:(BHT_IDX_MSB+1)]; // Store the PC tag
-        BHT[IF_ID_PC_curr_i[BHT_IDX_MSB:BHT_IDX_LSB]][2:1] <= updated_prediction; // Store the 2-bit prediction along with the tag
-        BHT[IF_ID_PC_curr_i[BHT_IDX_MSB:BHT_IDX_LSB]][0] <= updated_valid;        // Store the updated valid bit
+        BHT[IF_ID_PC_curr_i[BHT_IDX_MSB:BHT_IDX_LSB]][DBP_BHT_ENTRY_W-1:3] <= IF_ID_PC_curr_i[XLEN-1:(BHT_IDX_MSB+1)];
+        BHT[IF_ID_PC_curr_i[BHT_IDX_MSB:BHT_IDX_LSB]][2:1] <= updated_prediction;
+        BHT[IF_ID_PC_curr_i[BHT_IDX_MSB:BHT_IDX_LSB]][0] <= updated_valid;
       end
 
       // Update BTB entry if needed (when a branch is taken)
       if (enable_i && wen_BTB_i) begin
-        BTB[IF_ID_PC_curr_i[BTB_IDX_MSB:BTB_IDX_LSB]]  <= actual_target_i;  // Store the target address
+        BTB[IF_ID_PC_curr_i[BTB_IDX_MSB:BTB_IDX_LSB]] <= actual_target_i;
       end
     end
   end
@@ -109,32 +109,33 @@ module Dynamic_Branch_Predictor (
   assign prev_prediction = state_t'(IF_ID_prediction_i);
 
   // Read out the tag stored in the memory when enabled for a write.
-  assign write_tag = (enable_i) ? BHT[IF_ID_PC_curr_i[BHT_IDX_MSB:BHT_IDX_LSB]][DBP_BHT_ENTRY_W-1:3] : '0;
+  assign write_tag = (enable_i) ? BHT[IF_ID_PC_curr_i[BHT_IDX_MSB:BHT_IDX_LSB]][DBP_BHT_ENTRY_W-1:3]
+                                : '0;
 
   // Check if the write tags match for the current IF_ID_PC and the previous PC address in the cache.
   assign write_tags_match = (IF_ID_PC_curr_i[XLEN-1:(BHT_IDX_MSB+1)] == write_tag);
 
   always_comb begin
-    error              = 1'b0;  // Default error state.
+    error = 1'b0;  // Default error state.
     updated_prediction = STRONG_NOT_TAKEN;  // Default predict not taken.
-    updated_valid      = 1'b0;  // Default assume invalid.
+    updated_valid = 1'b0;  // Default assume invalid.
     case (prev_prediction)  // Update the new prediction based on the previous prediction.
       STRONG_NOT_TAKEN: begin
         updated_valid = 1'b1;  // Set the valid bit as it is a valid branch instruction
         if (write_tags_match) begin  // If the tags match, update the prediction.
-          updated_prediction = (actual_taken_i) ? WEAK_NOT_TAKEN : STRONG_NOT_TAKEN; // Go to weak not taken or stay in strong not taken
+          updated_prediction = (actual_taken_i) ? WEAK_NOT_TAKEN : STRONG_NOT_TAKEN;
         end else begin  // If the tags do not match, assume not taken.
           updated_prediction = WEAK_NOT_TAKEN;  // Default predict weak not taken.
         end
       end
       WEAK_NOT_TAKEN: begin  // Default state
         updated_valid = 1'b1;  // Set the valid bit as it is a valid branch instruction
-        updated_prediction = (actual_taken_i) ? WEAK_TAKEN : STRONG_NOT_TAKEN;     // Go to weak taken or back to strong not taken
+        updated_prediction = (actual_taken_i) ? WEAK_TAKEN : STRONG_NOT_TAKEN;
       end
       WEAK_TAKEN: begin
         updated_valid = 1'b1;  // Set the valid bit as it is a valid branch instruction
         if (write_tags_match) begin  // If the tags match, update the prediction.
-          updated_prediction = (actual_taken_i) ? STRONG_TAKEN : WEAK_NOT_TAKEN;    // Go to strong taken or go back to weak not taken
+          updated_prediction = (actual_taken_i) ? STRONG_TAKEN : WEAK_NOT_TAKEN;
         end else begin  // If the tags do not match, assume not taken.
           updated_prediction = WEAK_NOT_TAKEN;  // Default predict weak not taken.
         end
@@ -142,15 +143,15 @@ module Dynamic_Branch_Predictor (
       STRONG_TAKEN: begin
         updated_valid = 1'b1;  // Set the valid bit as it is a valid branch instruction
         if (write_tags_match) begin  // If the tags match, update the prediction.
-          updated_prediction = (actual_taken_i) ? STRONG_TAKEN : WEAK_TAKEN;     // Stay in strong taken or go back to weak taken
+          updated_prediction = (actual_taken_i) ? STRONG_TAKEN : WEAK_TAKEN;
         end else begin  // If the tags do not match, assume not taken.
           updated_prediction = WEAK_NOT_TAKEN;  // Default predict weak not taken.
         end
       end
       default: begin
-        updated_valid      = 1'b0;  // Default assume invalid.
+        updated_valid = 1'b0;  // Default assume invalid.
         updated_prediction = STRONG_NOT_TAKEN;  // Default predict not taken.
-        error              = 1'b1;  // Invalid prediction state.
+        error = 1'b1;  // Invalid prediction state.
       end
     endcase
   end
