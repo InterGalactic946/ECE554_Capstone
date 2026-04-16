648e1c77ebd9f86f81ea1333b4c12a70d39cf181
David Eapen <davidjeapen@gmail.com>
2026-04-15 21:51:17 -0500

    hopefully works?

--- FPGA_DMA/rtl/pcm_to_mem.sv
+++ FPGA_DMA/rtl/pcm_to_mem.sv
@@ -15,7 +15,11 @@ module pcm_to_mem (
     output logic write_en
 );
 
-    logic pcm_pos_second, pcm_neg_second;
+    logic pcm_1_pos_second, pcm_2_pos_second, pcm_1_neg_second, pcm_2_neg_second;
+    logic pcm_1_pos_done, pcm_2_pos_done, pcm_1_neg_done, pcm_2_neg_done;
+    logic aq_done;
+
+    assign aq_done = pcm_1_pos_done && pcm_2_pos_done && pcm_1_neg_done && pcm_2_neg_done;
 
     logic [127:0] pcm_buffer;
     logic [15:0] pcm_pos_1_first, pcm_pos_1_second, pcm_pos_2_first, pcm_pos_2_second;
@@ -24,14 +28,14 @@ module pcm_to_mem (
 
 
     always_comb begin
-        pcm_pos_1_first = (pcm_pos_valid_1 && !pcm_pos_second) ? pcm_pos_1 : pcm_buffer[127:112];
-        pcm_pos_1_second = (pcm_pos_valid_1 && pcm_pos_second) ? pcm_pos_1 : pcm_buffer[111:96];
-        pcm_pos_2_first = (pcm_pos_valid_2 && !pcm_pos_second) ? pcm_pos_2 : pcm_buffer[95:80];
-        pcm_pos_2_second = (pcm_pos_valid_2 && pcm_pos_second) ? pcm_pos_2 : pcm_buffer[79:64];
-        pcm_neg_1_first = (pcm_neg_valid_1 && !pcm_neg_second) ? pcm_neg_1 : pcm_buffer[63:48];
-        pcm_neg_1_second = (pcm_neg_valid_1 && pcm_neg_second) ? pcm_neg_1 : pcm_buffer[47:32];
-        pcm_neg_2_first = (pcm_neg_valid_2 && !pcm_neg_second) ? pcm_neg_2 : pcm_buffer[31:16];
-        pcm_neg_2_second = (pcm_neg_valid_2 && pcm_neg_second) ? pcm_neg_2 : pcm_buffer[15:0];
+        pcm_pos_1_first = (pcm_pos_valid_1 && !pcm_1_pos_second) ? pcm_pos_1 : pcm_buffer[127:112];
+        pcm_pos_1_second = (pcm_pos_valid_1 && pcm_1_pos_second) ? pcm_pos_1 : pcm_buffer[111:96];
+        pcm_pos_2_first = (pcm_pos_valid_2 && !pcm_2_pos_second) ? pcm_pos_2 : pcm_buffer[95:80];
+        pcm_pos_2_second = (pcm_pos_valid_2 && pcm_2_pos_second) ? pcm_pos_2 : pcm_buffer[79:64];
+        pcm_neg_1_first = (pcm_neg_valid_1 && !pcm_1_neg_second) ? pcm_neg_1 : pcm_buffer[63:48];
+        pcm_neg_1_second = (pcm_neg_valid_1 && pcm_1_neg_second) ? pcm_neg_1 : pcm_buffer[47:32];
+        pcm_neg_2_first = (pcm_neg_valid_2 && !pcm_2_neg_second) ? pcm_neg_2 : pcm_buffer[31:16];
+        pcm_neg_2_second = (pcm_neg_valid_2 && pcm_2_neg_second) ? pcm_neg_2 : pcm_buffer[15:0];
     end
     // PCM Buffer Layout {pcm_pos_1_first, pcm_pos_1_second, pcm_pos_2_first, pcm_pos_2_second, pcm_neg_1_first, pcm_neg_1_second, pcm_neg_2_first, pcm_neg_2_second}
     always_ff @( posedge clk, negedge rst_n ) begin
@@ -42,31 +46,91 @@ module pcm_to_mem (
         end
     end
 
-    always_ff @( posedge clk, negedge rst_n ) begin
-        if ( !rst_n ) begin
-            pcm_count <= '0;
-        end else if ((pcm_count == 3'd4)) begin
-            pcm_count <= '0;
-        end else if (ps_ready_for_data && (pcm_pos_valid_1 || pcm_pos_valid_2 || pcm_neg_valid_1 || pcm_neg_valid_2)) begin
-            pcm_count <= pcm_count + 1;
+    always_ff @( posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            pcm_1_pos_second <= 1'b0;
+        end else if (aq_done) begin
+            pcm_1_pos_second <= 1'b0; // Reset to first buffer after all data has been acquired
+        end else if (ps_ready_for_data && pcm_pos_valid_1) begin
+            pcm_1_pos_second <= 1'b1; // Toggle between first and second buffer for pcm_pos_1
         end
     end
 
     always_ff @( posedge clk, negedge rst_n) begin
-        if ( !rst_n ) begin
-            pcm_pos_second <= 0;
-            pcm_neg_second <= 0;
-        end else if (ps_ready_for_data && (pcm_pos_valid_1 || pcm_pos_valid_2)) begin
-            pcm_pos_second <= ~pcm_pos_second; // Toggle between first and second PCM samples
-        end else if (ps_ready_for_data && (pcm_neg_valid_1 || pcm_neg_valid_2)) begin
-            pcm_neg_second <= ~pcm_neg_second; // Toggle between first and second PCM samples
+        if (!rst_n) begin
+            pcm_2_pos_second <= 1'b0;
+        end else if (aq_done) begin
+            pcm_2_pos_second <= 1'b0; // Reset to first buffer after all data has been acquired
+        end else if (ps_ready_for_data && pcm_pos_valid_2) begin
+            pcm_2_pos_second <= 1'b1; // Toggle between first and second buffer for pcm_pos_2
         end
     end
+
+    always_ff @( posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            pcm_1_neg_second <= 1'b0;
+        end else if (aq_done) begin
+            pcm_1_neg_second <= 1'b0; // Reset to first buffer after all data has been acquired
+        end else if (ps_ready_for_data && pcm_neg_valid_1) begin
+            pcm_1_neg_second <= 1'b1; // Toggle between first and second buffer for pcm_neg_1
+        end
+    end
+
+    always_ff @( posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            pcm_2_neg_second <= 1'b0;
+        end else if (aq_done) begin
+            pcm_2_neg_second <= 1'b0; // Reset to first buffer after all data has been acquired
+        end else if (ps_ready_for_data && pcm_neg_valid_2) begin
+            pcm_2_neg_second <= 1'b1; // Toggle between first and second buffer for pcm_neg_2
+        end
+    end
+
+    always_ff @( posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            pcm_1_pos_done <= 1'b0;
+        end else if (aq_done) begin
+            pcm_1_pos_done <= 1'b0; // Reset done signal after all data has been acquired
+        end else if (ps_ready_for_data && pcm_pos_valid_1 && pcm_1_pos_second) begin
+            pcm_1_pos_done <= 1'b1; // Mark as done after second buffer is filled for pcm_pos_1
+        end
+    end
+
+    always_ff @( posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            pcm_2_pos_done <= 1'b0;
+        end else if (aq_done) begin
+            pcm_2_pos_done <= 1'b0; // Reset done signal after all data has been acquired
+        end else if (ps_ready_for_data && pcm_pos_valid_2 && pcm_2_pos_second) begin
+            pcm_2_pos_done <= 1'b1; // Mark as done after second buffer is filled for pcm_pos_2
+        end
+    end
+
+    always_ff @( posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            pcm_1_neg_done <= 1'b0;
+        end else if (aq_done) begin
+            pcm_1_neg_done <= 1'b0; // Reset done signal after all data has been acquired
+        end else if (ps_ready_for_data && pcm_neg_valid_1 && pcm_1_neg_second) begin
+            pcm_1_neg_done <= 1'b1; // Mark as done after second buffer is filled for pcm_neg_1
+        end
+    end
+
+    always_ff @( posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            pcm_2_neg_done <= 1'b0;
+        end else if (aq_done) begin
+            pcm_2_neg_done <= 1'b0; // Reset done signal after all data has been acquired
+        end else if (ps_ready_for_data && pcm_neg_valid_2 && pcm_2_neg_second) begin
+            pcm_2_neg_done <= 1'b1; // Mark as done after second buffer is filled for pcm_neg_2
+        end
+    end
+
 
     always_ff @( posedge clk, negedge rst_n ) begin
         if (!rst_n) begin 
             write_en <= 1'b0;
-        end else if (pcm_count == 3'd4) begin
+        end else if (aq_done) begin
             write_en <= 1'b1;
         end else if (!write_pending) begin
             write_en <= 1'b0;
@@ -76,7 +140,7 @@ module pcm_to_mem (
     always_ff @( posedge clk, negedge rst_n ) begin
         if ( !rst_n ) begin
             write_data <= '0;
-        end else if ((pcm_count == 3'd4)) begin
+        end else if (aq_done) begin
             write_data <= pcm_buffer;
         end
     end
