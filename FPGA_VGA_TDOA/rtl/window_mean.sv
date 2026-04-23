29887969c0e85eafcec1820a287a016d99a015ca
David Eapen <davidjeapen@gmail.com>
2026-04-22 20:08:14 -0500

    IP fifos

--- FPGA_VGA_TDOA/rtl/window_mean.sv
+++ FPGA_VGA_TDOA/rtl/window_mean.sv
@@ -22,19 +22,108 @@ module window_mean
     assign fifo_rden = fifo_full && data_valid;
     assign fifo_wren = data_valid;
 
-    My_FIFO #(
-        .DEPTH(WINDOW_SIZE),
-        .DATA_WIDTH(DATA_WIDTH)
-    ) fifo (
-        .clk(clk),
-        .rst_n(rst_n),
-        .rden(fifo_rden),
-        .wren(fifo_wren),
-        .i_data(data_in),
-        .o_data(fifo_out),
-        .full(fifo_full),
-        .empty()
-    );
+    // My_FIFO #(
+    //     .DEPTH(WINDOW_SIZE),
+    //     .DATA_WIDTH(DATA_WIDTH)
+    // ) fifo (
+    //     .clk(clk),
+    //     .rst_n(rst_n),
+    //     .rden(fifo_rden),
+    //     .wren(fifo_wren),
+    //     .i_data(data_in),
+    //     .o_data(fifo_out),
+    //     .full(fifo_full),
+    //     .empty()
+    // );
+
+    generate
+        if (WINDOW_SIZE == 8) begin
+            FIFO_8 fifo_8 (
+                .clock(clk),
+                .data(data_in),
+                .rdreq(fifo_rden),
+                .wrreq(fifo_wren),
+                .q(fifo_out),
+                .full(fifo_full)
+            );
+        end else if (WINDOW_SIZE == 16) begin
+            FIFO_16 fifo_16 (
+                .clock(clk),
+                .data(data_in),
+                .rdreq(fifo_rden),
+                .wrreq(fifo_wren),
+                .q(fifo_out),
+                .full(fifo_full)
+            );
+        end else if (WINDOW_SIZE == 32) begin
+            FIFO_32 fifo_32 (
+                .clock(clk),
+                .data(data_in),
+                .rdreq(fifo_rden),
+                .wrreq(fifo_wren),
+                .q(fifo_out),
+                .full(fifo_full)
+            );
+        end else if (WINDOW_SIZE == 64) begin
+            FIFO_64 fifo_64 (
+                .clock(clk),
+                .data(data_in),
+                .rdreq(fifo_rden),
+                .wrreq(fifo_wren),
+                .q(fifo_out),
+                .full(fifo_full)
+            );
+        end else if (WINDOW_SIZE == 128) begin
+            FIFO_128 fifo_128 (
+                .clock(clk),
+                .data(data_in),
+                .rdreq(fifo_rden),
+                .wrreq(fifo_wren),
+                .q(fifo_out),
+                .full(fifo_full)
+            );
+        end else if (WINDOW_SIZE == 256) begin
+            FIFO_256 fifo_256 (
+                .clock(clk),
+                .data(data_in),
+                .rdreq(fifo_rden),
+                .wrreq(fifo_wren),
+                .q(fifo_out),
+                .full(fifo_full)
+            );
+        end else if (WINDOW_SIZE == 512) begin
+            FIFO_512 fifo_512 (
+                .clock(clk),
+                .data(data_in),
+                .rdreq(fifo_rden),
+                .wrreq(fifo_wren),
+                .q(fifo_out),
+                .full(fifo_full)
+            );
+        end else if (WINDOW_SIZE == 1024) begin
+                FIFO_1024 fifo_1024 (
+                    .clock(clk),
+                    .data(data_in),
+                    .rdreq(fifo_rden),
+                    .wrreq(fifo_wren),
+                    .q(fifo_out),
+                    .full(fifo_full)
+                );
+        end else if (WINDOW_SIZE == 2048) begin
+            FIFO_2048 fifo_2048 (
+                .clock(clk),
+                .data(data_in),
+                .rdreq(fifo_rden),
+                .wrreq(fifo_wren),
+                .q(fifo_out),
+                .full(fifo_full)
+            );
+        end else begin
+            initial begin
+                $error("Unsupported WINDOW_SIZE: %0d. Supported sizes powers of 2 from 8 to 2048.", WINDOW_SIZE);
+            end
+        end
+    endgenerate
 
     always_ff @( posedge clk, negedge rst_n ) begin
         if (!rst_n) begin
