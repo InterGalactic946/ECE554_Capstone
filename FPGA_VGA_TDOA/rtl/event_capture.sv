1c889979c43af8e6a568c593ec1859e05903f2f4
David Eapen <davidjeapen@gmail.com>
2026-04-24 03:33:16 -0500

    old no timeout:

--- FPGA_VGA_TDOA/rtl/event_capture.sv
+++ FPGA_VGA_TDOA/rtl/event_capture.sv
@@ -222,8 +222,10 @@ module event_capture #(
                     if (|new_hits)
                         latch_new_hits = 1'b1;
 
-                    if (&(already_hit | above_threshold) || capture_done) begin
+                    if (&(already_hit | above_threshold)) begin
                         pulse_event_done = 1'b1;
+                        nxt_state = IDLE;
+                    end else if (capture_done) begin
                         nxt_state = IDLE;
                     end
                 end
