85a65e3c71114b8356b793b511d108cf3d10cc53
nateparmley <ndparmley@wisc.edu>
2026-04-28 01:10:39 -0500

    remove quadrant counter

--- FPGA_VGA_TDOA/rtl/quadrant_classifier.sv
+++ FPGA_VGA_TDOA/rtl/quadrant_classifier.sv
@@ -23,7 +23,7 @@ module quadrant_classifier #(
 
     logic [2:0]     classify_code;
 
-    logic [2:0]     quadrant_count;
+    //logic [2:0]     quadrant_count;
     logic [2:0]     prev_classify_code;
 
     logic [TSW-1:0] earliest_time;
@@ -74,7 +74,7 @@ module quadrant_classifier #(
             quadrant_valid <= 1'b0;
             quadrant_code  <= Q_UNKNOWN;
         // Checks if the quadrant stays the same for the 3 consecutive events after the classification
-        end else if (event_done && (quadrant_count == 3'b010)) begin
+        end else if (event_done) begin
                 quadrant_valid <= 1'b1;
                 quadrant_code  <= prev_classify_code;
         end
@@ -91,6 +91,8 @@ module quadrant_classifier #(
         end
     end
 
+    /*
+
     always_ff @(posedge clk, negedge rst_n) begin
         if (!rst_n) begin
             quadrant_count <= 3'b0;
@@ -100,5 +102,7 @@ module quadrant_classifier #(
             quadrant_count <= 3'b0;
         end
     end
+
+    */
 
 endmodule
